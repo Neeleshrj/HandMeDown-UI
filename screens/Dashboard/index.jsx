dd9ae41ead20198732589e24d49e6fb1c3cff792
Neelesh Ranjan Jha <neeleshranjan.jha2019@vitstudent.ac.in>
2022-04-18 03:47:56 +0530

    dashboard half done

--- screens/Dashboard/index.jsx
+++ screens/Dashboard/index.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, ScrollView } from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
 import axios from "axios";
 
 import Loading from "../Loading";
@@ -11,6 +12,7 @@ import Card from "../../ui/Card";
 //hooks
 import useEnv from "../../hooks/useEnv";
 import useAuth from "../../hooks/useAuth";
+import { heightPercentageToDP } from "react-native-responsive-screen";
 
 export default function Dashboard() {
   const [data, setData] = useState(null);
@@ -20,8 +22,6 @@ export default function Dashboard() {
 
   async function getData() {
     setLoading(true);
-    console.log(AuthContext.userId);
-    console.log(AuthContext.token)
     const url = baseURL + "/api/userInfo/getStats/" + AuthContext.userId;
     await axios
       .get(url, {
@@ -30,7 +30,6 @@ export default function Dashboard() {
         },
       })
       .then((res) => {
-        console.log(res.data)
         setData(res.data);
         // setTimeout(() => {
 
@@ -53,37 +52,103 @@ export default function Dashboard() {
   return (
     <View style={styles.container}>
       <View style={styles.upperContainer}>
-        <Card styling={styles.statCard}>
-          <View
-            style={{ flexDirection: "row", justifyContent: "space-between" }}
-          >
-            <Typography textSize="h3" textWeight="600">
-              Jobs Taken:{""}
-            </Typography>
-            <Typography textSize="h3" textWeight="600">
-              {data.tasksAccepted}
-            </Typography>
-          </View>
-        </Card>
-        <Card styling={styles.statCard}>
-          <View
-            style={{ flexDirection: "row", justifyContent: "space-between" }}
-          >
-            <Typography textSize="h3" textWeight="600">
-              Jobs Posted
-            </Typography>
-            <Typography textSize="h3" textWeight="600">
-              {data.tasksPosted}
-            </Typography>
-          </View>
-        </Card>
-      </View>
-      <View style={styles.lowerContainer}>
-        <Card>
+        <View
+          style={{
+            justifyContent: "center",
+            alignItems: "center",
+            width: "100%",
+            marginBottom: "5%",
+          }}
+        >
+          <Typography textSize="h1" textWeight="600" textColor="primary">
+            {data.totalIncome} Koins
+          </Typography>
           <Typography textSize="h3" textWeight="600">
             Total Income
           </Typography>
-        </Card>
+        </View>
+        <Typography textSize="h4" textWeight="600">
+          Statistics
+        </Typography>
+        <ScrollView
+          horizontal
+          contentContainerStyle={{ flexGrow: 1 }}
+          showsHorizontalScrollIndicator={false}
+        >
+          {/* <View> */}
+          {/* Tasks Accepted */}
+          <View style={{ width: 160 }}>
+            <Card styling={styles.statCard}>
+              <View>
+                <Ionicons
+                  name="list-outline"
+                  color="#000"
+                  size={heightPercentageToDP(4)}
+                />
+                <Typography textSize="h2" textWeight="600">
+                  {data.tasksAccepted}
+                </Typography>
+                <Typography textSize="h6" textWeight="600">
+                  Jobs
+                </Typography>
+                <Typography textSize="h6" textWeight="600">
+                  Accepted
+                </Typography>
+              </View>
+            </Card>
+          </View>
+
+          {/* Tasks Posted */}
+          <View style={{ width: 160, height: 150 }}>
+            <Card styling={styles.statCard}>
+              <View>
+                <Ionicons
+                  name="add-circle-outline"
+                  color="#000"
+                  size={heightPercentageToDP(4)}
+                />
+                <Typography textSize="h2" textWeight="600">
+                  {data.tasksPosted}
+                </Typography>
+                <Typography textSize="h6" textWeight="600">
+                  Jobs
+                </Typography>
+                <Typography textSize="h6" textWeight="600">
+                  Posted
+                </Typography>
+              </View>
+            </Card>
+          </View>
+
+          {/* Tasks Completed */}
+          <View style={{ width: 160, height: 150 }}>
+            <Card styling={styles.statCard}>
+              <View>
+                <Ionicons
+                  name="checkmark-circle-outline"
+                  color="#000"
+                  size={heightPercentageToDP(4)}
+                />
+                <Typography textSize="h2" textWeight="600">
+                  {data.tasksPosted}
+                </Typography>
+                <Typography textSize="h6" textWeight="600">
+                  Jobs
+                </Typography>
+                <Typography textSize="h6" textWeight="600">
+                  Completed
+                </Typography>
+              </View>
+            </Card>
+          </View>
+
+          {/* </View> */}
+        </ScrollView>
+      </View>
+      <View style={styles.lowerContainer}>
+        <Typography textSize="h6" textWeight="600">
+          On Going Jobs
+        </Typography>
       </View>
     </View>
   );
@@ -96,24 +161,22 @@ const styles = StyleSheet.create({
     paddingHorizontal: "2%",
   },
   upperContainer: {
-    // flexDirection: 'row',
-    backgroundColor: "red",
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
   },
   statCard: {
-    padding: "4%",
+    padding: "10%",
     borderColor: "#F50057",
     borderWidth: 1,
     marginVertical: "4%",
-    width: "70%",
-    height: 100,
-    flex: 1,
+    height: 150,
+    width: 150,
     justifyContent: "center",
   },
   lowerContainer: {
-    backgroundColor: "green",
     flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
